030bd2df3544469bb9049dbf2302e6c7c669c10e
aegires <kosty.zabivnoff@gmail.com>
2023-02-14 19:08:29 +0300

    Added alphabets, numbers, specials

---
+++ src/Literals.cs
@@ -0,0 +1,9 @@
+namespace NLiterals;
+
+public static class Literals
+{
+    public static readonly string UppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static readonly string LowercaseAlphabet = UppercaseAlphabet.ToLower();
+    public static readonly string Numbers = "0123456789";
+    public static readonly string Specials = "!\"#$%&\\'()*+,-./:;<=>?@[]^_{|}~`№";
+}
